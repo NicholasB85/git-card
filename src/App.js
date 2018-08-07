e74b75324429539a85d244910754fe5ee194e967
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-08-07 14:58:10 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 // import logo from './logo.svg';
 import "./App.css";
+import { Button } from 'semantic-ui-react'
+import { Card, Icon } from 'semantic-ui-react'
+
+const extra = 
+  <a>
+    <Icon name='user' />
+    16 Friends
+  </a>
+
 
 class App extends Component {
   state = {
@@ -21,14 +30,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>button</button>
+        <Button onClick={this.handleClick}>button</Button>
         {this.state.active ? (
-          <div>
-            <h1>{this.state.users.login}</h1>
-            <img src={this.state.users.avatar_url}/>
-            <h2>Public Repos: {this.state.users.public_repos}</h2>
-            <h2>Followers: {this.state.users.followers}</h2>
-          </div>
+          // <div>
+          //   <h1>{this.state.users.login}</h1>
+          //   <img src={this.state.users.avatar_url}/>
+          //   <h2>Public Repos: {this.state.users.public_repos}</h2>
+          //   <h2>Followers: {this.state.users.followers}</h2>
+          // </div>
+          <Card 
+          image={this.state.users.avatar_url}
+          header={this.state.users.login}
+          meta={"Public Repos: " + this.state.users.public_repos}
+          description={"Folowers: " + this.state.users.followers}
+          extra={extra}
+        />
         ) : null}
       </div>
     );
