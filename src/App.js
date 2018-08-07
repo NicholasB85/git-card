1f65f5d11c3e7831edfa62f3f5a5b2fcaf2343ec
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-08-07 12:30:23 -0400

    third part of assesment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   state = {
@@ -8,16 +8,29 @@ class App extends Component {
     active: false
   };
   handleClick = () => {
-    fetch("https://api.github.com/users/NicholasB85")
-    .then(res => res.json())
-    .then(data => {
-      this.setState({users: data, active: true})
-    })
-  }
+    if(this.state.active === true){
+        this.setState({users: null, active: false})
+    }else{
+      fetch("https://api.github.com/users/NicholasB85")
+        .then(res => res.json())
+        .then(data => {
+          this.setState({ users: data, active: true });
+        });
+      }
+  };
   render() {
     return (
-      <button onClick={this.handleClick}>button</button>
-
+      <div>
+        <button onClick={this.handleClick}>button</button>
+        {this.state.active ? (
+          <div>
+            <h1>{this.state.users.login}</h1>
+            <img src={this.state.users.avatar_url}/>
+            <h2>Public Repos: {this.state.users.public_repos}</h2>
+            <h2>Followers: {this.state.users.followers}</h2>
+          </div>
+        ) : null}
+      </div>
     );
   }
 }
